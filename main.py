af35418d40c71bd9cb0d244280fabfe06f6e0de4
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-06-03 19:03:32 +0200

    Add vigenere

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import itertools
 
-cipher = input("Cipher: ").lower() or ""
-alphabet = input("Alphabet [a-z]: ") or "abcdefghijklmnopqrstuvwxyz"
-words = [x.strip("\n") for x in open(input("Dictionary [words.txt]: ") or "words.txt", "r").readlines()]
+cipher = input("Cipher: ").upper() or ""
+alphabet = input("Alphabet [A-Z]: ").upper() or "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+words = [x.strip("\n").upper() for x in open(input("Dictionary [words.txt]: ") or "words.txt", "r").readlines()]
+keys = [x.strip("\n").upper() for x in open(input("Keys [keys.txt]: ") or "keys.txt", "r").readlines()]
 
 
 def reverse(string):
@@ -10,12 +11,7 @@ def reverse(string):
 
 
 def replace(string, salphabet):
-    estring = ""
-    for char in string:
-        if char in alphabet:
-            char = salphabet[alphabet.index(char)]
-        estring += char
-    return estring
+    return "".join([salphabet[alphabet.index(x)] if x in alphabet else x for x in string])
 
 
 def caesar(string, shift):
@@ -26,9 +22,23 @@ def atbash(string):
     return replace(string, alphabet[::-1])
 
 
+def vigenere(string, key):
+    key = [x for x in key.upper() if x in alphabet]
+    i = 0
+    estring = ""
+    for char in string:
+        if char in alphabet:
+            char = alphabet[caesar(alphabet, alphabet.index(key[i % len(key)])).index(char)]
+            i += 1
+        estring += char
+    return estring
+
+
 functions = [(reverse,), (atbash,)]
 for x in range(1, len(alphabet)):
     functions.append((caesar, x))
+for x in keys:
+    functions.append((vigenere, x))
 
 for r in range(1, 8):
     print("==", r, "==")
@@ -37,6 +47,6 @@ for r in range(1, 8):
         string = cipher
         for y in x:
             string = y[0](string, *y[1:])
-        rs.add(string)
-    rs = sorted(rs, key=lambda x: sum(len(y) for y in words if y in x))
+        rs.add((string,) + tuple((x1[0].__name__, *x1[1:]) for x1 in x))
+    rs = sorted(rs, key=lambda x: sum(len(y) for y in words if y in x[0]))
     print(*rs[-3:][::-1], sep="\n")
